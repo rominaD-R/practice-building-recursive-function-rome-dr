1d38bca1524b8fbf7ca96d28ade29f14466626ec
Romina Diaz-Rivero <romina@wustl.edu>
2025-10-20 15:44:09 -0500

    WIP on string reversing

---
+++ recursive.js
@@ -0,0 +1,21 @@
+// 1. Write a function named reverseString that takes a string as input.
+
+// 2. The function should:
+//      ○ Return the string itself if it contains only one character (base case).
+//      ○ Use recursion to reverse the string by concatenating the last character of the string with the result of the 
+//      recursive call on the remaining part of the string (recursive case).
+
+// 3. Test the function with various string inputs to verify its correctness
+
+
+function reverseString(string) {
+    if (string.length == 1 || string.length == 0) {
+        console.log(string[0]);
+        return string[0];
+    }
+    console.log(string[string.length - 1]);
+    // string.pop;
+    return reverseString(string.slice(0, (string.length - 1)));
+}
+
+reverseString('abcd');
